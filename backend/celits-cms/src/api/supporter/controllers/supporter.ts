664b3eb8fe76fd334ee02c9a3756e24fb35b1b94
Upvista Digital <upvistadigital@gmail.com>
2025-07-04 22:44:28 +0500

    Delete backend/celits-cms directory

--- backend/celits-cms/src/api/supporter/controllers/supporter.ts
+++
@@ -1,7 +0,0 @@
-/**
- * supporter controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::supporter.supporter');
--- backend/celits-cms/src/api/supporter/routes/supporter.ts
+++
@@ -1,7 +0,0 @@
-/**
- * supporter router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::supporter.supporter');
